e05d733e4123ed8d7232817231ee387462b5676a
matias <matiascabezas2007@gmail.com>
2026-04-07 18:55:05 -0400

    fix: corrige feature 1 saludo que no se guardo correctamente

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+
+const app = express();
+
+
+
+// Middleware para leer JSON
+
+app.use(express.json());
+
+
+
+// Ruta GET básica
+
+app.get('/', (req, res) => {
+
+    res.send('Hola mundo 🌍');
+
+});
+
+
+
+// Ruta GET con parámetro
+
+app.get('/saludo/:nombre', (req, res) => {
+
+    const nombre = req.params.nombre;
+
+    res.send(`Hola ${nombre} bienvenido a este mundo`);
+});
+
+//`Hola ${nombre}, bienvenido a este mundo`
+
+// Ruta POST
+
+app.post('/usuario', (req, res) => {
+
+    const data = req.body;
+
+    res.json({
+
+        mensaje: 'Usuario creado',
+
+        data: data
+
+    });
+
+});
+
+
+
+// Levantar servidor
+
+app.listen(3000, () => {
+
+    console.log('Servidor corriendo en localhost:3000');
+
+});
