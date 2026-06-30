fe1811be0473492104270c9c34f920bac3d348e3
MatiasCa27 <ma.cabezast@duocuc.cl>
2026-06-29 21:12:17 -0400

    Remover integracion directa de CloudWatch

--- logger.js
+++ logger.js
@@ -1,26 +1,13 @@
 const winston = require('winston');
-const WinstonCloudWatch = require('winston-cloudwatch');
 
 const logger = winston.createLogger({
   level: 'info',
   format: winston.format.combine(
     winston.format.timestamp(),
-    winston.format.json()        // logs en formato JSON estructurado
+    winston.format.json()
   ),
   transports: [
-
-    // Transport 1: consola (para ver logs localmente)
-    new winston.transports.Console(),
-
-    // Transport 2: CloudWatch (para ver logs en AWS)
-    new WinstonCloudWatch({
-      logGroupName: '/evparcial/app',        // nombre del Log Group en CloudWatch
-      logStreamName: 'express-logs',         // nombre del stream dentro del grupo
-      awsRegion: 'us-east-1',
-      awsAccessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      awsSecretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-      jsonMessage: true
-    })
+    new winston.transports.Console()
   ]
 });
 
