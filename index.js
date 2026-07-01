f4f57e087e8b41e720e1cc86e7df19879d99bf8c
Rayen B <ra.beltran@duocuc.cl>
2026-06-30 22:41:30 -0400

    Actualiza mensaje de bienvenida

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
 // Ruta GET básica
 app.get('/', (req, res) => {
 
-    res.send('Hola mundo 🌍 - version final test 3');
+    res.send('Hola mundo 🌍 - version final test 4');
 
 });
 
